af30463586f42cb8a6f313a3640b2376cc8d0c28
Thomas Fox <thomas.allan.fox@gmail.com>
2015-01-08 22:01:38 -0500

    Added scoring system.

--- pong.py
+++ pong.py
@@ -11,6 +11,7 @@ class Game:
     self.height = 480
     self.width = 640
     self.screen = pygame.display.set_mode([self.width, self.height])
+    self.score = [0,0]
 
   def event_update(self):
     for event in pygame.event.get():
@@ -21,6 +22,10 @@ class Game:
           paddle1.y -= paddle1.speed
         if event.key == pygame.K_DOWN:
           paddle1.y += paddle1.speed
+        if event.key == pygame.K_w:
+          paddle2.y -= paddle2.speed
+        if event.key == pygame.K_s:
+          paddle2.y += paddle2.speed
          
 
 class Ball:
@@ -40,25 +45,52 @@ class Paddle():
 
 game = Game()  
 ball = Ball()
+myfont = pygame.font.SysFont("monospace", 15)
 paddle1 = Paddle(10, 240)
 paddle2 = Paddle(610, 240)
 
 while True:
-  if ball.x - ball.radius  < 0 or ball.x + ball.radius > game.width:
+  if ball.x - ball.radius  < 0:
     ball.x = int(game.height/2)
     ball.y = int(game.width/2)
+    ball.speed = [-5,0]
+    game.score[1] += 1
+  if ball.x + ball.radius > game.width: 
+    ball.x = int(game.height/2)
+    ball.y = int(game.width/2)
+    ball.speed = [5,0] 
+    game.score[0] += 1
   if ball.y - ball.radius < 0 or ball.y + ball.radius > game.height:
     ball.speed[1] = -ball.speed[1]
   if paddle1.y < 0:
     paddle1.y = 0
   if paddle1.y + paddle1.height > game.height:
     paddle1.y = game.height - paddle1.height
+  #check if ball hits paddl1
   if ball.x - ball.radius < paddle1.x + paddle1.width and ball.y < paddle1.y + paddle1.height and ball.y > paddle1.y:
     ball.speed[0] = -ball.speed[0] 
+    if ball.speed[0] < 20:
+      ball.speed[0] += 1
+    if ball.speed[1] < 20:
+      ball.speed[1] += 1  
+  #check if ball hits top of paddle1
   if ball.y + ball.radius > paddle1.y and ball.x > paddle1.x and ball.x < paddle1.x + paddle1.width:
     ball.speed[1] = -ball.speed[1]
+  #check if ball hits bottom of paddle1
+  if ball.y - ball.radius < paddle1.y + paddle1.height and ball.x > paddle1.x and ball.x < paddle1.x + paddle1.width:
+    ball.speed[1] = -ball.speed[1]
+  #check if ball hits paddle2
   if ball.x + ball.radius > paddle2.x and ball.y < paddle2.y + paddle2.height and ball.y > paddle2.y:
+    if ball.speed[0] < 20:
+      ball.speed[0] += 1
+    if ball.speed[1] < 20:
+      ball.speed[1] += 1
     ball.speed[0] = -ball.speed[0]
+  #check if ball hits top of paddle2
+  if ball.y + ball.radius > paddle2.y and ball.x > paddle2.x and ball.x < paddle2.x + paddle2.width:
+    ball.speed[1] = -ball.speed[1]
+  if ball.y - ball.radius < paddle2.y + paddle2.height and ball.x > paddle2.x and ball.x < paddle2.x + paddle2.width:
+    ball.speed[1] = -ball.speed[1]
 
   
   game.event_update()
@@ -66,6 +98,11 @@ while True:
   
   ball.x += ball.speed[0]
   ball.y += ball.speed[1]
+
+  paddle2.y = ball.y - int(paddle2.height/2)
+
+  label = myfont.render("Score:" + str(game.score[0]) + " to " + str(game.score[1]), 1, WHITE)
+  game.screen.blit(label, (240, 20))
   
   pygame.draw.rect(game.screen, WHITE, [paddle1.x, paddle1.y, paddle1.width, paddle1.height])
   pygame.draw.rect(game.screen, WHITE, [paddle2.x, paddle2.y, paddle2.width, paddle2.height])
